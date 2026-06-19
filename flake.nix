12900abc6438269bbde0f279b7f0e0813f983026
Simon Inns <simoninns@fastmail.com>
2026-06-19 22:20:05 +0200

    Update qtnodes to 2026-06-08 and nix to 26.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   # Upstream dependencies for the flake
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
     qtnodes = {
       url = "github:paceholder/nodeeditor";
